4a4e4cccffe022e1a6b768e88451f3ac2d1aac70
DaronP <jandresperez1@yahoo.com>
2020-08-30 22:47:12 -0600

    Confirmacion por email
    Elimine la contraseña de mi correo porque pues xd

--- src/js/Usuario.js
+++ src/js/Usuario.js
@@ -1,5 +1,20 @@
 const Sequelize = require('sequelize')
 const { QueryTypes } = require('sequelize')
+const nodemailer = require('nodemailer')
+var Dialogs = require('dialogs')
+const dialogs = Dialogs()
+
+//Generacion de codigo de 7 digitos
+var vCode = Math.floor((Math.random() * 9999999) + 1000000);
+
+//Transportador de correo
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jaandresperez4556@gmail.com',
+        pass: ''
+    }
+})
 
  var db_config ={
      host: 'localhost',
@@ -34,17 +49,54 @@ const Users = sequelize.define('Usuario',{
         type: Sequelize.STRING
     },
     email:{
+        type: Sequelize.STRING
+    },
+    password:{
         type: Sequelize.STRING,
         primaryKey: true,
         autoIncrement: false
-    },
-    password:{
-        type: Sequelize.STRING
     }
 })
 
 
 Users.sync()
+
+//Mandando email
+var adMail = Users.findOne({where: {first_name: 'admin'}}).then(c =>{
+    dialogs.alert('Email enviado a: ' + c.email)
+
+    var mailOptions = {
+        from: 'per16362@uvg.edu.gt',
+        to: c.email,
+        subject: 'Codigo de Verificacion',
+        text: 'El codigo de verificacion es: ' + vCode
+    }
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Email enviado a: ' + info.response)
+            
+    
+        }
+    });
+    
+});
+if(adMail === null){
+    console.log('Nel mijo')
+}
+
+
+Users.count({where: {firstName: 'admin'}}).then(c => {
+    if(c >= 1){
+        
+
+    }else{
+        alert("Por favor, ingrese un administrador")
+        window.location="../html/SignUp.html"
+    }
+})
 
 function addUser(){
     console.log("BOTON APACHADO")
@@ -52,29 +104,33 @@ function addUser(){
     var lastName = document.getElementById('lastName').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var cod = document.getElementById('codigo').value;
 
-    Users.count({where: {email: email}}).then(c => {
-        if(c == 0 && firstName != "" && lastName != "" && email != "" && password != ""){
-            Users.create({
-                first_name: firstName,
-                last_name: lastName,
-                email: email,
-                password: password,
-                type: 'admin'
-            });
-            alert("Usuario creado con exito.")
-            window.location="../html/login.html"
-        }
-        else{
 
-            throw error
-        }
-        return c
-    }).catch(err => {
-        return err;
-    })
+    if(vCode == parseInt(cod)){    
 
-    
+        Users.count({where: {email: email}}).then(c => {
+            if(c == 0 && firstName != "" && lastName != "" && email != "" && password != ""){
+                Users.create({
+                    first_name: firstName,
+                    last_name: lastName,
+                    email: email,
+                    password: password,
+                    type: 'admin'
+                });
+                alert("Usuario creado con exito.")
+                window.location="../html/login.html"
+            }
+            else{
+                throw error
+            }
+            return c
+        }).catch(err => {
+            return err;
+        })
+    }else{
+        //window.location="../html/SignUp.html"
+    }
 }
 
 function checkUser(){
@@ -86,7 +142,7 @@ function checkUser(){
             throw error;
         }
         else{
-            console.log("ALV")
+            console.log("done.")
             console.log(c)
             alert("Bienvenido.")
             window.location="../html/mainmenu.html"
@@ -99,6 +155,10 @@ function checkUser(){
     })
     
 }
+
+
+
+
 
 document.getElementById("Submit").onClick = addUser();
 document.getElementById("signIn").onClick = checkUser();
